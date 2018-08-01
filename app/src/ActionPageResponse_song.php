4124a556e00f77f172e484513b2550de73889b23
3545664848 <38251131+3545664848@users.noreply.github.com>
2018-08-01 13:48:39 +0800

    这是我的第一个动态页面

---
+++ app/src/ActionPageResponse_song.php
@@ -0,0 +1,17 @@
+<?php
+namespace cn\atd3\response;
+
+use suda\core\Request;
+
+class ActionPageResponse_song extends \suda\core\Response
+{
+    public function onRequest(Request $request)
+    {
+        $view = $this->page('action-page-song'); // 创建一个页面并使用 action-page.tpl.html 作为模板
+        $view->set('ip',$request->ip()); // 获取IP并压入模板
+        $view->set('time',date('Y-m-d H:i:s',time())); // 压入时间
+        debug()->info('日志信息');
+        $view->render();  // 渲染模板显示出来
+    }
+}
+
